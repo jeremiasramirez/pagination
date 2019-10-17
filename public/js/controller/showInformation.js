6f7846beab447109aec769b8eac7bb38f40a71d4
Jeremias ramirez <jereramirez499@gmail.com>
2019-10-17 14:21:38 -0400

    Agregando logica y belleza de codigo a la app web - (el maldito final)

--- public/js/controller/showInformation.js
+++ public/js/controller/showInformation.js
@@ -1,36 +1,59 @@
 app.controller("showData", ["$scope", "$http", ($scope, $http)=>{
 
-
-
-
-
     //path of view info
+
     $scope.pathInformation = "view/viewInformation.html";
+
+
+    //pagination logic
+
     $scope.infoArray = [];
-
-    //pagination
-
     $scope.pos = 5;
+    $scope.len = 0;
+    $scope.counter = 1;
 
 
-
-
-
-
+    //htt request to .json data
     $http({
-
 
         method: "get",
         url: "/pagination/data/data.json"
 
-
     }).then((resp)=>{
 
-        console.log(resp.data);
         $scope.infoArray = resp.data;
 
+        $scope.len = Math.ceil((resp.data.length) / 5);
 
-    })
+    });
+
+
+    $scope.nexts = function(){
+
+        if($scope.counter < $scope.len){
+
+            // incrementing counter & position of data
+            $scope.counter +=1;
+            $scope.pos += 5;
+
+        }
+
+    };
+
+    $scope.backs = function(){
+        //decrementing counter & position of data
+
+        if($scope.counter > 1){
+
+            $scope.counter -=1;
+            $scope.pos -= 5;
+
+        }
+
+    };
+
+
+
 
 
 }]);
